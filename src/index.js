b05b6749d541cb5a8eb282fa19bc43dd56975928
Zizeko Anastasiya <nastjaznf@gmail.com>
2019-10-09 10:20:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+    if (parseInt(first) == 0 || parseInt(second) == 0) {
+        return '0';
+    }
+
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+    let result = [];
+
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 0; j < second.length; j++) {
+
+            if (!result[i + j]) {
+                result[i + j] = 0;
+            }
+            let mltpl = first[i] * second[j];
+            result[i + j] += mltpl;
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
+            }
+            let digit = result[i] % 10;
+            let movingPart = parseInt(result[i] / 10);
+            result[i + 1] += movingPart;
+            result[i] = digit;
+        }
+    }
+
+    return result.reverse().join('');
 }
